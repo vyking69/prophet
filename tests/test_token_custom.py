156b9fdfd6b6f7da90bcc9d59219a3aeaea4a804
vyking69 <139842940+vyking69@users.noreply.github.com>
2023-08-21 16:13:18 -0700

    5 tier test
    made sure that an NFT could go all the way to level 5 in the tests successfully.

--- tests/test_token_custom.py
+++ tests/test_token_custom.py
@@ -13,20 +13,31 @@ def test_level_up(nft_env_values):
     latest_nft = nft.next_id()
 
     # mint the nft
+    working_token_id = nft.next_id()
     nft.mint(w)
-
-    # add tokens to w for level up operation
-    previous_token_amount = token.see_balance_of(w)
+    
     tokens_needed_for_level_up = 10
-    token.mint(w, tokens_needed_for_level_up)
 
-    assert token.see_balance_of(w) == previous_token_amount + tokens_needed_for_level_up, "tokens added doesn't match expectations"
 
-    # begin level up process
-    token.trigger_level_up(latest_nft)
+    # level up NFT to max level
+    for i in range(1,5):
+        # track tier progression
+        starting_tier = nft.see_nft_tier(working_token_id)
 
-    assert token.see_balance_of(w) == previous_token_amount, "amount of tokens burned not matching expectations"
+        # add tokens to w for level up operation
+        previous_token_amount = token.see_balance_of(w) # keep test atomic!
+        tokens_needed_for_level_up = tokens_needed_for_level_up * 10 if i != 1 else 10
+        token.mint(w, tokens_needed_for_level_up)
 
+        print(previous_token_amount, tokens_needed_for_level_up, token.see_balance_of(w))
+        assert token.see_balance_of(w) == previous_token_amount + tokens_needed_for_level_up, "tokens added doesn't match expectations"
+
+        # begin level up process
+        token.trigger_level_up(latest_nft)
+
+        print(previous_token_amount, tokens_needed_for_level_up, token.see_balance_of(w))
+        assert token.see_balance_of(w) == previous_token_amount, "amount of tokens burned not matching expectations"
+        assert nft.see_nft_tier(working_token_id) == starting_tier + 1, "NFT tier did not increase by one"
 
 
 
